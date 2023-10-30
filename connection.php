b042ff8ad3d1e188b787c576530b8a2ab98e40d8
MahesaYogha <116375160+MahesaYogha@users.noreply.github.com>
2023-10-30 18:19:53 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+$conn =  mysqli_connect("localhost:3308", "root", "" ,"wad_project3");
+$result= mysqli_query($conn, "SELECT * FROM students");
+echo "<br/>";
+var_dump(mysqli_fetch_object($result));
+
+?>
